0c08143ec190fa96a89e4558033a284bd8e3f05b
Eduardo Mollo <eduardo.mollo@outlook.it>
2021-02-18 22:01:57 +0100

    Load variables. If conditions

---
+++ caramel.js
@@ -0,0 +1,159 @@
+class Caramel {
+
+    // Syntax constants
+    CM_START_VAR_CHAR = '{{';
+    CM_END_VAR_CHAR = '}}';
+
+    constructor() {
+
+    }
+
+    /**
+     * Method that returns all the DOM elements with a specific attribute name
+     * @param {string} attribute - The attribute to search for
+     * @returns {any[]} A list of DOM elements
+     */
+    getAllElementsWithAttribute(attribute) {
+        return document.querySelectorAll('[' + attribute + ']');
+    }
+
+    /**
+     * Method that removes all the spaces in a string
+     * @param {string} string - The string to edit
+     * @returns {string} The original string without spaces
+     */
+    removeSpaces(string) {
+        return string.replace(/(>)\s+|\s+(<)/g, "$1$2");
+    }
+
+    /**
+     * Method that removes all the HTML comments inside the page
+     */
+    removeHTMLComments() {
+        const pageHTML = document.documentElement.innerHTML;
+        document.documentElement.innerHTML = pageHTML.replace(/<!--[\s\S]*?-->/g, '');
+    }
+
+    /**
+     * Method that scans an HTML string and looks for matches
+     * @param {string} elementHTML - Element HTML to scan
+     * @returns {object[]} A list of objects that contains the data found inside the elements
+     */
+    findOccurrences(elementHTML) {
+
+        // Initializing variables
+        let matches, data, param, toReplace;
+        let regex = new RegExp(`${this.CM_START_VAR_CHAR}(.*?)${this.CM_END_VAR_CHAR}`, 'g');
+        let toReplaceFound = [];
+
+        // Get all the occurrences and build the list of objects
+        while (matches = regex.exec(elementHTML)) {
+
+            // Matches data
+            param = matches[1].trim();
+            toReplace = matches[0];
+
+            // Check for data
+            try {
+                data = eval(param);
+                if (!data) {
+                    console.error(`The object ${param} is undefined.`);
+                }
+            } catch {
+                console.error(`The object ${param} is undefined.`);
+            }
+
+            // Object building
+            let object = {
+                element: elementHTML,
+                param,
+                toReplace,
+                data
+            };
+            
+            toReplaceFound.push(object);
+
+        }
+
+        return toReplaceFound;
+
+    }
+
+    /**
+     * Method that manages the cmIf elements
+     */
+    loadConditions() {
+
+        // Get all the cmIf elements
+        const elements = this.getAllElementsWithAttribute('cmif');
+
+        // Check for the elements to hide
+        for (let element of elements) {
+            let cmIf = element.getAttribute('cmif');
+            try {
+                let checkCondition = eval(cmIf);
+                if (!checkCondition) {
+                    element.remove();
+                }
+            } catch(error) {
+                console.error(`The condition ${cmIf} is not valid.`);
+            }
+            element.removeAttribute('cmif');
+        }
+
+    }
+
+    /**
+     * Method that manages the variables inside the DOM elements
+     */
+    loadVariables() {
+        
+        // Get all the DOM elements
+        const elements = document.documentElement.getElementsByTagName('*');
+
+        for (let element of elements) {
+
+            // Get all the occurrences for the element
+            let elementHTML = element.innerHTML;
+            let toReplaceFound = this.findOccurrences(elementHTML);
+
+            // Replace all the occurrences found with the data
+            let newElement = elementHTML;
+            for (let item of toReplaceFound) {
+                newElement = this.removeSpaces(newElement).split(item.toReplace).join(item.data);
+            }
+
+            // Replace original DOM element
+            element.innerHTML = newElement;
+
+        }
+        
+    }
+
+    /**
+     * Main method that loads Caramel
+     */
+    load() {
+        this.removeHTMLComments();
+        this.loadConditions();
+        this.loadVariables();
+    }
+
+}
+
+window.onload = function() {
+
+    // Caramel load
+    var caramel = new Caramel();
+    caramel.load();
+
+    // Caramel update after an AJAX call
+    if (window.jQuery) {
+        $.ajaxSetup({
+            complete: function (xhr, settings) {
+                caramel.load();
+            }
+        });
+    }
+
+}
---
+++ object-examples.js
@@ -0,0 +1,88 @@
+var complexObject = {
+    "id": "0001",
+    "type": "donut",
+    "name": "Cake",
+    "ppu": 0.55,
+    "batters":
+        {
+            "batter":
+                [
+                    { "id": "1001", "type": "Regular" },
+                    { "id": "1002", "type": "Chocolate" },
+                    { "id": "1003", "type": "Blueberry" },
+                    { "id": "1004", "type": "Devil's Food" }
+                ]
+        },
+    "topping":
+        [
+            { "id": "5001", "type": "None" },
+            { "id": "5002", "type": "Glazed" },
+            { "id": "5005", "type": "Sugar" },
+            { "id": "5007", "type": "Powdered Sugar" },
+            { "id": "5006", "type": "Chocolate with Sprinkles" },
+            { "id": "5003", "type": "Chocolate" },
+            { "id": "5004", "type": "Maple" }
+        ]
+    };
+
+var nestedObject =  { "test": { id: "1001", "type": "Everything works fine :-)" } }
+
+var arrayOfObjects =  [
+    { "id": "1001", "type": "Regular" },
+    { "id": "1002", "type": "Chocolate" },
+    { "id": "1003", "type": "Blueberry" },
+    { "id": "1004", "type": "Devil's Food" }
+];
+
+var arrayExample = [
+    "Orange",
+    "Banana",
+    "Apple",
+    "Kiwi"
+];
+
+var objectWithArrayOfObjects = {
+    "batter":
+        [
+            { "id": "1001", "type": "Regular" },
+            { "id": "1002", "type": "Chocolate" },
+            { "id": "1003", "type": "Blueberry" },
+            { "id": "1004", "type": "Devil's Food" }
+        ]
+}
+
+var simpleVariable = 'My favourite animal is the walrus';
+
+var apiExampleList = {};
+
+var tryVariable = {test: 1};
+
+var googleURL = 'https://www.google.com/';
+
+var nestedArray = [
+    {
+        title: 'Test 1',
+        subTitle: 'Sub 1',
+        items: [
+            { title: 'Test 1.1', subTitle: 'Sub 1.1' },
+            { title: 'Test 1.2', subTitle: 'Sub 1.2' }
+        ]
+    },
+    {
+        title: 'Test 2',
+        subTitle: 'Sub 2',
+        items: [
+            { 
+                title: 'Test 2.1',
+                subTitle: 'Sub 2.1',
+                items: [
+                    { title: 'Test 2.1.1' },
+                    { title: 'Test 2.1.2' }
+                ]
+            },
+            { title: 'Test 2.2', subTitle: 'Sub 2.2' }
+        ]
+    }
+];
+
+var arrayOfNumbers = [1, 2, 3, 4];
